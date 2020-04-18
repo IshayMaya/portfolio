e3a6ba7972a7e9e0242f2d0a780156f6f48966a3
ishayMaya <ssaymama@gmail.com>
2020-04-18 17:17:03 +0300

    attempt to scroll on mobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from './Layout'
 import './App.scss';
 
 function App() {
+
+  useEffect(() => {
+    window.scrollTo(0,1);
+  },[])
+  
   return (
     <div className="App">
       <Layout />
